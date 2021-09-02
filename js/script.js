5ab321f8e74d7df742c034f63d34d7ec99a6e81d
Hujaifa21 <21hujaifa@gmail.com>
2021-09-02 23:04:58 +0600

    maybe the final push

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const errorDiv = document.getElementById("error");
-
+// fetching data from api 
 const loadBooks = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
   searchField.value = '';
   const url = `https://openlibrary.org/search.json?q=${searchText}`
   fetch(url)
-    .then(res => res.json())
-    .then(data => loadBook(data.docs))
-    // error handling
+  .then(res => res.json())
+  .then(data => loadBook(data.docs))
+  // error handling
+  const errorDiv = document.getElementById("error");
   if (searchText === "") {
     errorDiv.innerText = "Search field cannot be empty.";
     return;
@@ -17,28 +17,20 @@ const loadBooks = () => {
 }
 
 const loadBook = books => {
-  
-  const bookContainer = document.getElementById('book-container')
-  // clear dom
-  bookContainer.innerHTML = '';
   const totalResult = document.getElementById('total-result')
-  totalResult.innerText = `total results: ${books.length}`
-  // Error Handing
-  
+  totalResult.innerText = `Total ${books.length} books match your search result`
+  const bookContainer = document.getElementById('book-container')
+  bookContainer.innerHTML = '';
   books.forEach(book => {
-    if (book.message === "Not Found") {
-      errorDiv.innerText = "NO Result Found";
-      return;
-    }
     const div = document.createElement('div')
     div.innerHTML = `
     <div class="col">
-      <div class="card vh-100 shadow-lg">
+      <div class="card h-100 shadow-lg">
         <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="rounded-start" style="height: 300px;" alt="..."" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${book?.title}</h5>
           <p class="card-text">Author: ${book?.author_name}</p>
-          <p class="card-text">Published by ${book?.publisher[0]}</p>
+          <p class="card-text">Publisher: ${book?.publisher[0]}</p>
         </div>
         <div class="card-footer text-center">
           <small class="text-muted">First published: ${book?.first_publish_year}</small>
@@ -50,14 +42,4 @@ const loadBook = books => {
   });
 }
 
-
-
-const displaySearchResult = books => {
-
-  const result = ${books.numFound}
-  if(result==="0"){
-      totalResult.innerText =`No result Found`
-  }
-  else{
-      totalResult.innerText =`Total Search Result ${books.numFound}`
-  }
+// End-----------ed
