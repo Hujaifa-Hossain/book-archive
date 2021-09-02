314c7a69a3c4556aa0367f481b56bf4da68a44b3
Hujaifa21 <21hujaifa@gmail.com>
2021-09-02 21:01:49 +0600

    so many things added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,47 @@
+const errorDiv = document.getElementById("error");
+
 const loadBooks = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
   searchField.value = '';
   const url = `https://openlibrary.org/search.json?q=${searchText}`
   fetch(url)
-  .then(res => res.json())
-  .then(data => loadBook(data.docs))
+    .then(res => res.json())
+    .then(data => loadBook(data.docs))
+    // error handling
+  if (searchText === "") {
+    errorDiv.innerText = "Search field cannot be empty.";
+    return;
+  }
+  errorDiv.innerText = '';
 }
 
 const loadBook = books => {
+  
   const bookContainer = document.getElementById('book-container')
+  // clear dom
+  bookContainer.innerHTML = '';
+  const totalResult = document.getElementById('total-result')
+  totalResult.innerText = `total results: ${books.length}`
+  // Error Handing
+  
   books.forEach(book => {
-    // const imgUrl = `${book.cover_i}`
-    console.log(book)
+    if (book.message === "Not Found") {
+      errorDiv.innerText = "NO Result Found";
+      return;
+    }
     const div = document.createElement('div')
     div.innerHTML = `
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-6">
-          <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="img-fluid rounded-start vh-100" alt="...">
+    <div class="col">
+      <div class="card vh-100 shadow-lg">
+        <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="rounded-start" style="height: 300px;" alt="..."" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${book?.title}</h5>
+          <p class="card-text">Author: ${book?.author_name}</p>
+          <p class="card-text">Published by ${book?.publisher[0]}</p>
         </div>
-        <div class="col-md-6">
-          <div class="card-body">
-            <h5 class="card-title">${book?.title}</h5>
-            <p class="card-text">by ${book?.author_name}</p>
-            <p class="card-text"><small class="text-muted">First published ${book?.first_publish_year}</small></p>
-          </div>
+        <div class="card-footer text-center">
+          <small class="text-muted">First published: ${book?.first_publish_year}</small>
         </div>
       </div>
     </div>
@@ -35,13 +51,13 @@ const loadBook = books => {
 }
 
 
-{/* <div class="card">
-<img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top vh-100" alt="...">
-<div class="card-body">
-  <h5 class="card-title">${book.title}</h5>
-  <p class="card-text">${book.title}</p>
-</div>
-<div class="card-footer text-center">
-  <small>First published ${book?.first_publish_year}</small>
-</div>
-</div> */}
+
+const displaySearchResult = books => {
+
+  const result = ${books.numFound}
+  if(result==="0"){
+      totalResult.innerText =`No result Found`
+  }
+  else{
+      totalResult.innerText =`Total Search Result ${books.numFound}`
+  }
